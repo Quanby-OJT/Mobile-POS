66656328dc78e3ae5eb03c7927e1e632a465ab08
clarence <vegaclarence77@gmail.com>
2025-02-10 18:32:55 +0800

    Add verification image and label

--- lib/views/otp_page.dart
+++ lib/views/otp_page.dart
@@ -10,6 +10,40 @@ class OtpPage extends StatefulWidget {
 class _OtpPageState extends State<OtpPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        forceMaterialTransparency: false,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/verification_badge.png',
+                width: 150,
+                height: 150,
+                ),
+                Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Text(
+                  'Verification',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87
+                  ),
+                  ),
+              ),
+            ],
+          )
+        ),
+      ),
+    );
   }
 }
