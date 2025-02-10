f401047d01035b6b9a791d498afd1afeffb11d3f
clarence <vegaclarence77@gmail.com>
2025-02-10 14:10:43 +0800

    Add login label

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -25,6 +25,47 @@ class _LoginPageState extends State<LoginPage> {
               ),
             )
           ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white
+              ),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10, left: 50),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold
+                          ),
+                          ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 50 ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Welcome back! Please login to your \nCesar's account.",
+                          style: TextStyle(
+                            color: Colors.black26
+                          ),
+                          ),
+                      ),
+                    ),
+                  ],
+                )
+              ),
+            )
+            )
         ],
       ),
     );
