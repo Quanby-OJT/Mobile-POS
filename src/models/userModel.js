2c036bb6e39f8fb7411c5fa2365399d2acb0b4fe
Pogs789 <ceslorica15@gmail.com>
2025-02-11 09:44:45 +0800

    Developing Multi-Factor Authentication and Login Authentication to SUpabase

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,39 +2,38 @@ const supabase = require('../config/supabaseClient');
 
 class UserModel {
     static async addUser(name, email) {
-        const { data, error } = await supabase.from('users').insert([{ name, email }]);
+        const { data, error } = await supabase.from('user').insert([{ name, email }]);
         if (error) throw new Error(error.message);
         return data;
     }
 
     static async getAllUsers() {
-        const { data, error } = await supabase.from('users').select('*');
+        const { data, error } = await supabase.from('user').select('*');
         if (error) throw new Error(error.message);
         return data;
     }
 
-        static async attemptLoginAuth(email, password) {
-            const {data, error} = await supabase.from('Users').select('email', 'activation', 'password', 'status').is('email', email)
-            if (error) throw new error(error.message)
-            return data;
-        }
-
-        static async generateOTP()
-        {
-            let otp = Math.floor(100000 + Math.random() * 900000);
-            const timestamp = new Date()
-            const { data, error } = await supabase.from('Users').insert([{ two_fa_code: otp }]);
-            if (error) throw new Error(error.message);
-            return data;
-        }
-
-        static async attemptOTPAuth(email)
-        {
-            const { data, error } = await supabase.from('Users').select('two_fa_code').in('email', email);
-            if (error) throw new Error(error.message);
-            return data;
-        }
+    static async attemptLoginAuth(email, password) {
+        const {data, error} = await supabase.from('Users').select('email', 'activation', 'password', 'status').is('email', email)
+        if (error) throw new error(error.message)
+        return data;
+    }
     
+    static async generateOTP()
+    {
+        let otp = Math.floor(100000 + Math.random() * 900000);
+        const timestamp = new Date()
+        const { data, error } = await supabase.from('Users').insert([{ two_fa_code: otp }]);
+        if (error) throw new Error(error.message);
+        return data;
+    }
+
+    static async attemptOTPAuth(email)
+    {
+        const { data, error } = await supabase.from('Users').select('two_fa_code').in('email', email);
+        if (error) throw new Error(error.message);
+        return data;
+    }
 }
 
 module.exports = UserModel;
